83003623082f6e828afeff81b44d27d34baf6a92
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-02-19 12:46:39 -0500

    feat(servicios): agregar responsive al ModalButton

--- app/(client)/servicios/components/ModalButton.jsx
+++ app/(client)/servicios/components/ModalButton.jsx
@@ -20,7 +20,7 @@ export default function ModalClick({ text, fondo, title, serviceName }) {
   const hideModal = () => {
     backgroundRef.current.classList.add('fade-out-bg');
     modalRef.current.classList.add('fade-out-modal');
-    
+
     setTimeout(() => {
       backgroundRef.current.classList.add('hidden');
       backgroundRef.current.classList.remove('fade-in', 'fade-out-bg');
@@ -63,17 +63,44 @@ export default function ModalClick({ text, fondo, title, serviceName }) {
         onClick={(e) => e.stopPropagation()}
         className="bg-black flex relative text-white rounded-2xl overflow-hidden bg-gradient-to-bl from-[#B721FF] to-[#21D4FD] p-8 gap-8"
       >
-        <button onClick={hideModal} className="absolute top-4 right-4">X</button>
+        <button onClick={hideModal} className="absolute top-4 right-4">
+          X
+        </button>
         <div>
-          <p className="font-bold text-2xl text-center mb-4 max-w-sm">{title}</p>
+          <p className="font-bold text-2xl text-center mb-4 max-w-sm">
+            {title}
+          </p>
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-            <Input label="Nombre" type="text" name="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-            <Input label="Teléfono" type="text" name="telefono" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
-            <Input label="Correo" type="email" name="correo" value={correo} onChange={(e) => setEmail(e.target.value)} />
-            <button className="bg-[#a121fd] font-bold p-4 rounded-lg" type="submit">¡EMPIEZA YA!</button>
+            <Input
+              label="Nombre"
+              type="text"
+              name="nombre"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+            />
+            <Input
+              label="Teléfono"
+              type="text"
+              name="telefono"
+              value={telefono}
+              onChange={(e) => setTelefono(e.target.value)}
+            />
+            <Input
+              label="Correo"
+              type="email"
+              name="correo"
+              value={correo}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button
+              className="bg-[#a121fd] font-bold p-4 rounded-lg"
+              type="submit"
+            >
+              ¡EMPIEZA YA!
+            </button>
           </form>
         </div>
-        <div className="flex flex-col justify-center items-center gap-8">
+        <div className="flex-col justify-center items-center gap-8 hidden md:flex">
           <img className="max-w-60 max-h-52 w-auto" src={fondo} alt="" />
           <p className="font-medium max-w-48 text-center">{text}</p>
         </div>
@@ -85,7 +112,9 @@ export default function ModalClick({ text, fondo, title, serviceName }) {
 function Input({ label, type, name, value, onChange }) {
   return (
     <div className="flex gap-2 justify-between items-center">
-      <label className="font-semibold shrink-0 basis-20" htmlFor={name}>{label}</label>
+      <label className="font-semibold shrink-0 basis-20" htmlFor={name}>
+        {label}
+      </label>
       <input
         className="p-1 rounded-md grow text-black"
         id={name}
