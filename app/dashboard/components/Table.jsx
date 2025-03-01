a73939c1f9d82161e40ff2a83910eba98f5d079c
JefersonDominguez <jeffersondominguez082@gmail.com>
2025-03-01 09:55:41 -0500

    tabla arreglada

--- app/dashboard/components/Table.jsx
+++ app/dashboard/components/Table.jsx
@@ -1,7 +1,7 @@
 export default function Table({ headers, data, onDelete, onUpdate }) {
   const headersList = headers.map((header, index) => {
     return (
-      <th key={index} className="p-2">
+      <th key={index} className="p-2 bg-[#8c52ff] text-white rounded-xl">
         {header.toUpperCase()}
       </th>
     );
@@ -12,47 +12,38 @@ export default function Table({ headers, data, onDelete, onUpdate }) {
 
     for (let i = 0; i < headers.length; i++) {
       row.push(
-        <td key={i} className="p-2">
+        <td key={i} className="p-2 rounded-xl">
           {dataRow[headers[i]]}
         </td>
       );
     }
     row.push(
-      <td key={50}>
-        <button 
-          className="bg-[#dc3545] text-white rounded-md px-3 py-2 m-2"
-          onClick={() => onDelete(dataRow.id)}
-          hidden={onDelete == false}
-        >
-          Eliminar
+      <td key={50} className="rounded-xl text-center">
+        <button onClick={() => onDelete(dataRow.id)} hidden={onDelete == false}>
+          <img src="/dashboard/trash-icon.svg" className="w-8" alt="" />
         </button>
-        <button 
-          className="bg-[#ffc107] rounded-md px-3 py-2 m-2"
-          onClick={() => onUpdate(dataRow.id)}
-          hidden={onUpdate == false}
-        >
-          Editar
+        <button onClick={() => onUpdate(dataRow.id)} hidden={onUpdate == false}>
+          <img src="/dashboard/edit-icon.svg" className="w-8" alt="" />
         </button>
       </td>
     );
 
     return (
-      <tr key={dataRow.id} className="odd:bg-[#f2f2f2] h-[56px]">
+      <tr
+        key={dataRow.id}
+        className="odd:bg-[#e5e5e5] even:bg-[#f6f6f6] h-[56px]"
+      >
         {row}
       </tr>
     );
   });
 
   return (
-    <table className="min-w-full bg-white ">
+    <table className="min-w-full bg-white border-separate border-spacing-1 font-medium">
       <thead>
-        <tr>
-          {headersList}
-        </tr>
+        <tr>{headersList}</tr>
       </thead>
-      <tbody>
-        {dataList}
-      </tbody>
+      <tbody>{dataList}</tbody>
     </table>
   );
 }
