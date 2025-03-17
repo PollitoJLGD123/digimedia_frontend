c3dbe6e6c48c5077c28b55151cd046dfb4b1502d
Kevin <keving.kpg@gmail.com>
2025-03-17 09:18:13 -0500

    Cambio de conexión a Back local

--- app/dashboard/users/services/user.service.jsx
+++ app/dashboard/users/services/user.service.jsx
@@ -1,7 +1,7 @@
 "use client"
 
-//const api_url = "http://127.0.0.1:8000/api/user";
-const api_url = "https://back.digimediamkt.com/api/user";
+const api_url = "http://127.0.0.1:8000/api/user";
+//const api_url = "https://back.digimediamkt.com/api/user";
 
 import { getCookie } from "cookies-next";
 import auth_service from "./auth.service";
