4895f3498907698a98989d67bf1dc25ba4e1718e
Gogoz22 <130242754+Gogoz22@users.noreply.github.com>
2025-03-31 11:03:57 -0500

    Cambios para el blog

--- app/edition/components/Footer.jsx
+++ app/edition/components/Footer.jsx
@@ -1,10 +1,37 @@
-
-import React from 'react'
+import Link from 'next/link';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <footer>
+        <div className="mainFooter">
+          <div className="footerContenido">
+
+
+            <div className="imgFooter my-4">
+              <img
+                src="/headerFooter/logoFooter.webp"
+                alt="Digimedia"
+                width="250px"
+                height="120px"
+                loading="lazy"
+              />
+            </div>
+  </div>
+          <div className="barraFooter">
+            <hr />
+          </div>
+          <div className="rucFooter text-white">
+            <div className="ruc">
+              <p>RUC: 20605116559</p>
+            </div>
+            <div className="derechosFooter">
+              <p>@digimedia.com. Derechos reservados</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
--- app/edition/plantillas/plantilla1/page.jsx
+++ app/edition/plantillas/plantilla1/page.jsx
@@ -2,9 +2,9 @@
 
 export default function EditPlantilla1() {
     return (
-        <div>
-            <h2>Editando Plantilla 1</h2>
-            <p>Aquí puedes editar la plantilla 1.</p>
+        <div className="text-black"> 
+            <h2 className="size-11">Editando Plantilla 1</h2>
+            <p className= "font-serif"> xd  Aquí puedes editar la plantilla 1.</p>
             {/* Aquí va el formulario de edición de la plantilla 1 */}
         </div>
     );
