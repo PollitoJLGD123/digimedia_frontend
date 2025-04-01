c95b811e646cfcae1cb64fc98411759bd62614d3
Jose Luis <joseluisjlgd123@gmail.com>
2025-04-01 10:19:39 -0500

    Informacion de la plantilla body

--- app/edition/plantillas/plantilla2/page.jsx
+++ app/edition/plantillas/plantilla2/page.jsx
@@ -20,10 +20,65 @@ const PageContent = () => {
     url_image: "/blog/fondo_blog_extend",
   });
 
+  const [formEncabezadoBody, setFormEncabezadoBody] = useState({
+    titulo: "Tu título impactante aquí",
+    descripcion:
+      "Frase secundaria y llamativa",
+    fecha: '2025-03-31 12:00:57',
+    url_image1: "/blog/fondo_blog_extend.png",
+  });
+
+  const [formInfoBody, setFormInfoBody] = useState({
+    titulo: "El Factor Sorpresa y Distinción",
+    descripcion:"Las letras de neón LED permiten personalizar la imagen de tu local, haciendo que el nombre de tu bar sea visible desde lejos. Un diseño llamativo puede convertirse en un sello distintivo y en un punto de referencia para los clientes.",
+    titulo1: "Ambiente y Experiencia Visual",
+    descripcion1:"La iluminación juega un papel crucial en la atmósfera de un bar. Los colores vibrantes y cálidos del neón LED pueden transformar un espacio ordinario en un entorno acogedor e instagrameable.",
+    titulo2: "Eficiencia Energética y Durabilidad",
+    descripcion2:"A diferencia del neón tradicional, las luces LED son más eficientes, consumen menos energía y tienen una vida útil más prolongada.",
+    titulo3: "Marketing y Atracción de Clientes",
+    descripcion3:"Un letrero de neón LED bien diseñado es una herramienta de marketing poderosa, capaz de captar la atención y aumentar la visibilidad de tu local.",
+  });
+
+  const [formCommendBody, setFormCommendBody] = useState({
+    titulo: "Consejos para Elegir el Letrero Perfecto",
+    texto1:
+    "Opta por colores que reflejen la personalidad de tu bar.",
+    texto2:
+    "Elige un diseño legible y atractivo.",
+    texto3:
+    "Considera el lugar de instalación para maximizar su impacto.",
+  });
+
+  const [formGaleryBody, setFormGaleryBody] = useState({
+    url_image2: "/blog/blog-2.jpg",
+    url_image3: "/blog/blog-2.jpg"
+  });
+
   return (
     <>
-      <FormHeader dataHeader={dataHeader} />
-      <FormFooter formFooter={formFooter} />
+      <FormHeader 
+        dataHeader={dataHeader} 
+        setFormData={setDataHeader}
+      />
+
+      <FormBody1
+        formCommendBody = {formCommendBody}
+        setFormCommendBody = {setFormCommendBody}
+        
+        formInfoBody = {formInfoBody}
+        setFormInfoBody = {setFormInfoBody}
+
+        formGaleryBody = {formGaleryBody}
+        setFormGaleryBody = {setFormGaleryBody}
+
+        formEncabezadoBody = {formEncabezadoBody}
+        setFormEncabezadoBody = {setFormEncabezadoBody}
+      />
+      
+      <FormFooter
+        formFooter={formFooter}
+        setFormData={setFormFooter}
+      />
     </>
   );
 };
