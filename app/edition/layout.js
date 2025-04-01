4c0b4e6e7fa9d9d7c46b5221db7f108d48119f41
PieroCatacora <pierocatacorayt13@gmail.com>
2025-04-01 11:44:35 -0500

    terminado

--- app/edition/layout.js
+++ app/edition/layout.js
@@ -1,4 +1,3 @@
-
 "use client"
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -15,41 +14,45 @@ export default function EditionLayout({ children }) {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      
+  
       <div className="flex flex-1">
-        <div className="w-44 bg-purple-800 text-white p-4 flex flex-col justify-between">
+        {/* Barra lateral fija */}
+        <div className="w-44 bg-purple-800 text-white p-4 flex flex-col justify-between sticky top-0 h-screen">
           <div>
             <h2 className="text-lg text-center p-4">Estructura</h2>
             <div className="space-y-3">
               <button
-                className={`w-full py-2 rounded-md text-center ${selectedSection === 'header' ? 'bg-black' : 'bg-purple-700'} 
-                  hover:bg-yellow hover:scale-105 hover:shadow-lg transition-all duration-200`}
+                className={`w-full py-2 rounded-md text-center ${
+                  selectedSection === 'header' ? 'bg-black' : 'bg-purple-700'
+                } hover:bg-yellow hover:scale-105 hover:shadow-lg transition-all duration-200`}
                 onClick={() => handleSectionClick('header')}
               >
                 Header
               </button>
               <button
-                className={`w-full py-2 rounded-md text-center ${selectedSection === 'body' ? 'bg-black' : 'bg-purple-700'} 
-                  hover:bg-purple-800 hover:scale-105 hover:shadow-lg transition-all duration-200`}
+                className={`w-full py-2 rounded-md text-center ${
+                  selectedSection === 'body' ? 'bg-black' : 'bg-purple-700'
+                } hover:bg-purple-800 hover:scale-105 hover:shadow-lg transition-all duration-200`}
                 onClick={() => handleSectionClick('body')}
               >
                 Body
               </button>
               <button
-                className={`w-full py-2 rounded-md text-center ${selectedSection === 'footer' ? 'bg-black' : 'bg-purple-700'} 
-                  hover:bg-purple-800 hover:scale-105 hover:shadow-lg transition-all duration-200`}
+                className={`w-full py-2 rounded-md text-center ${
+                  selectedSection === 'footer' ? 'bg-black' : 'bg-purple-700'
+                } hover:bg-purple-800 hover:scale-105 hover:shadow-lg transition-all duration-200`}
                 onClick={() => handleSectionClick('footer')}
               >
                 Footer
               </button>
             </div>
           </div>
-
+  
           <button className="w-full py-2 mt-4 bg-[#4CAF50] hover:bg-[#3cf743] text-white rounded-md">
             Guardar
           </button>
         </div>
-
+        
         <div className="flex-1 p-4 bg-white">
           {children} 
         </div>
