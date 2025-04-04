1769578ec6dfbf8cc43febabc7bf0769b85a7872
Jose Luis <joseluisjlgd123@gmail.com>
2025-04-04 03:35:47 -0500

    Agregar imagen en header hecho

---
+++ app/edition/services/Cloud.jsx
@@ -0,0 +1,35 @@
+
+import React from 'react'
+import axios from 'axios'
+
+const API_DELETE = "/api/delete_image"
+
+const Cloud = {
+    deleteImage: async function deleteImage(public_id) {
+        try {
+            const response = await axios.post(API_DELETE, 
+                { 
+                    "public_id": public_id
+                },
+                {   
+                    headers:{
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+            console.log(response);
+            if (response.status === 200) {
+                return response.data.result;
+            }
+            else {
+                return null;
+            }
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    }
+}
+
+export default Cloud;
