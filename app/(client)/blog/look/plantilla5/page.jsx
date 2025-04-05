62e4367ca33f32261467399ac426b62d65796554
francesca silva <frannysilva@hotmail.com>
2025-04-05 12:11:06 -0500

    plantilla 5

--- app/(client)/blog/look/plantilla5/page.jsx
+++ app/(client)/blog/look/plantilla5/page.jsx
@@ -26,13 +26,18 @@ export default function Page() {
             descripcion: "Un letrero de neón LED bien diseñado es una herramienta de marketing poderosa, capaz de captar la atención y aumentar la visibilidad de tu local.",
         }
     ]
+      
+    
 
 
 
 
     return (
         <div>
-            <Header url_image={"/blog/blog-3.jpg"} />
+            <Header url_image={"/blog/blog-3.jpg"} tituloPrincipal = {"MARKETING Y GESTIÓN DIGITAL"}
+              tituloSecundario ={"¡Impulsa tu marca al éxito digital!"} 
+              descripcion = {"El marketing y la gestión digital son tus aliados para potenciar el éxito de tu marca en el mundo digital"}
+            />
             <div className="container mx-auto px-4 py-12 relative bg-gradient-to-r text-black min-h-screen w-full">
                 <div className="hidden lg:block w-20 xl:w-24 2xl:w-32 bg-gradient-to-b from-red-700 via-sky to-blue-800 fixed left-0 top-0 h-full -z-10"></div>
 
@@ -185,7 +190,7 @@ export default function Page() {
                             <div className="h-3 bg-gradient-to-r from-indigo-500 via-purple-500 to-indigo-500"></div>
                         </div>
 
-                <Footer url_image1={"/blog/blog-5.jpg"} url_image2={"/blog/blog-8.jpg"} url_image3={"/blog/blog-2.jpg"} />
+                <Footer url_image1={"/blog/blog-5.jpg"} url_image2={"/blog/blog-8.jpg"} url_image3={"/blog/blog-7.jpg"} />
 
             </div>
         </div>
