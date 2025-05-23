f6848968b355898f7f9d80d89325d5272b0e8718
Gogoz22 <130242754+Gogoz22@users.noreply.github.com>
2025-05-23 11:18:32 -0500

    Normalizando resolucion de imagenes main de servicios

--- app/(client)/servicios/components/Main.jsx
+++ app/(client)/servicios/components/Main.jsx
@@ -3,12 +3,12 @@ export default function Main({ title, subtitle, text, image, className = "" }) {
     <main
       className={`flex flex-col-reverse items-center justify-between max-w-7xl m-auto md:flex-row px-5 py-8 md:px-24 md:py-24 gap-0 md:gap-12 ${className}`}
     >
-      <div className="text-center md:text-left md:max-w-xl w-full mt-0 md:mt-5">
+      <div className="text-center md:text-left md:max-w-xl w-full mt-0 md:mt-0">
         <h1 className="text-[#523194] font-semibold text-3xl my-4 font-title md:text-5xl">{title}</h1>
         <h2 className="text-[#ff037f] font-semibold text-1xl my-2 uppercase md:text-xl">{subtitle}</h2>
         <p className="text-justify font-bold uppercase text-2xs">{text}</p>
       </div>
-      <img className="max-w-[90%] md:max-w-[50%] mb-4 md:mb-0" src={image || "/placeholder.svg"} alt="" />
+      <img className="max-w-[90%] md:max-w-[35%] mb-4 md:mb-0" src={image || "/placeholder.svg"} alt="" />
     </main>
   )
 }
